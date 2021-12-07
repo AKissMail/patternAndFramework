b15368184cf87ad9ab650e25a613bd91d690596d
Thomas K <thomas.krohnfuss@stud.th-luebeck.de>
2021-12-07 21:55:32 +0100

    Projektstrukturen für Server-App vorbereitet und Ordner und Dateien angelegt

---
+++ src/main/java/de/gruppeo/wise2122_java_server/MibquizzzApplication.java
@@ -0,0 +1,13 @@
+package de.gruppeo.wise2122_java_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MibquizzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MibquizzzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/gruppeo/wise2122_java_server/mibquizzz/MibquizzzApplicationTests.java
@@ -0,0 +1,13 @@
+package de.gruppeo.wise2122_java_server.mibquizzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MibquizzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
