e875fd00777f47a28330cd0abdbb17b9e14bbf70
Thomas K <thomas.krohnfuss@stud.th-luebeck.de>
2021-12-09 18:08:41 +0100

    schema.sql an JPA Anforderung (kein CamelCase und klein geschrieben) angepasst

--- src/main/java/de/gruppeo/wise2122_java_server/docker/mysql/schema.sql
+++ src/main/java/de/gruppeo/wise2122_java_server/docker/mysql/schema.sql
@@ -7,7 +7,7 @@ CREATE DATABASE IF NOT EXISTS mibquizzz CHARACTER SET utf8mb4 COLLATE utf8mb4_un
 # 1. Spielerprofile
 create table player
 (
-    playerid         bigint auto_increment
+    playerid         bigint(20) auto_increment
         primary key,
     username         varchar(20)                                                           not null,
     password         varchar(100)                                                          not null,
@@ -25,43 +25,43 @@ create table player
 # 2. Kategorien der Fragen
 CREATE TABLE quizCategory
 (
-    quizCategoryID int auto_increment
+    quizcategoryid bigint(20) auto_increment
         primary key,
     categoryName   varchar(255) not null,
-    constraint quizCategory_quizCategoryID_uindex
-        unique (quizCategoryID)
+    constraint quizCategory_quizCategoryid_uindex
+        unique (quizcategoryid)
 )
     comment 'Enthält verschiedene Fachgebiete';
 
 # 3. Tabellen mit den Fragen erstellen
 create table quizQuestions
 (
-    quizQuestionID  int auto_increment
+    quizquestionid  bigint auto_increment
         primary key,
     question        text                                                        not null,
     correctAnswer   varchar(255)                                                not null,
     falseAnswer1    varchar(255)                                                not null,
     falseAnswer2    varchar(255)                                                not null,
     falseAnswer3    varchar(255)                                                not null,
-    fk_quizCategory int                                                         null,
+    fk_quizcategory bigint                                                      null,
     difficulty      enum ('Fachkraft', 'Ingenieur', 'Prof') default 'Fachkraft' not null,
     constraint quizQuestions_quizQuestionID_uindex
-        unique (quizQuestionID),
+        unique (quizquestionid),
     constraint quizQuestions_quizCategory_quizCategoryID_fk
-        foreign key (fk_quizCategory) references quizCategory (quizCategoryID)
+        foreign key (fk_quizcategory) references quizCategory (quizcategoryid)
 )
     comment 'Enthält die Quizfragen';
 
 # 4. Highscore Table
 create table quizHighscore
 (
-    quizHighscoreID int auto_increment
+    quizHighscoreid bigint auto_increment
         primary key,
-    fk_playerid     int                                   not null,
+    fk_playerid     bigint                                   not null,
     highscore       int       default 0                   null,
     lastUpdate      timestamp default current_timestamp() not null on update current_timestamp(),
     constraint quizHighscore_quizHighscoreid_uindex
-        unique (quizHighscoreID),
+        unique (quizHighscoreid),
     constraint quizHighscore_player_playerid_fk
         foreign key (fk_playerid) references player (playerid)
             on delete cascade
