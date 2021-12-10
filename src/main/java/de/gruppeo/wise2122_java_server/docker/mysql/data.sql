9b5bd13f6ffd9a2334db7510cf8946951fd66c12
Thomas K <thomas.krohnfuss@stud.th-luebeck.de>
2021-12-10 23:27:44 +0100

    Umsetzung JPA bzw. Spring Vorgaben - aus sampleData.sql wurde data.sql. Außerdem noch Beispieldaten für die Highscoretabelle ergänzt

--- src/main/java/de/gruppeo/wise2122_java_server/docker/mysql/data.sql
+++ src/main/java/de/gruppeo/wise2122_java_server/docker/mysql/data.sql
@@ -1,6 +1,8 @@
 # 1. Sample Spieler anlegen
 INSERT INTO mibquizzz.player (playerid, username, password, currentscore, fk_quizHighscore, thumbnail, currentstatus)
 VALUES (1, 'todeskralle', 'fingerweg!', 100, 0, null, 'offline');
+INSERT INTO mibquizzz.player (playerid, username, password, currentscore, fk_quizHighscore, thumbnail, currentstatus)
+VALUES (1, 'bree luce', 't0d35kr4113', 500, 0, null, 'offline');
 
 # 2. Beispielkategorien anlegen
 INSERT INTO mibquizzz.category (quizcategoryid, categoryname)
@@ -20,4 +22,8 @@ INSERT INTO mibquizzz.questions (quizquestionid, question, correctAnswer, falseA
 VALUES (2, 'Woran erkennt man eine sichere Webseite?', 'Am kleinen Schloss neben der URL',
         'Am optischen Gesamteindruck des Seite', 'Am Impressum: Der Firmensitz sollte in Deutschland sein',
         'Wie aktuell die Seite ist', 3, 'Fachkraft');
+
+#4. Highscore Beispieldaten
+INSERT INTO mibquizzz.highscore (quizHighscoreID, fk_playerID, highscore, lastUpdate) VALUES (1, 1, 500, '2021-12-08 12:16:45');
+INSERT INTO mibquizzz.highscore (quizHighscoreID, fk_playerID, highscore, lastUpdate) VALUES (2, 2, 350, '2021-12-10 22:25:08');
 
