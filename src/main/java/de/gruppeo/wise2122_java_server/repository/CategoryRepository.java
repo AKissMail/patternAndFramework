2601571f6e0c5eb9bb700dd705c6a6fda897d42d
Thomas K <thomas.krohnfuss@stud.th-luebeck.de>
2021-12-11 00:42:11 +0100

    CategoryRepository "repariert"

--- src/main/java/de/gruppeo/wise2122_java_server/repository/CategoryRepository.java
+++ src/main/java/de/gruppeo/wise2122_java_server/repository/CategoryRepository.java
@@ -4,5 +4,4 @@ import de.gruppeo.wise2122_java_server.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
 }
