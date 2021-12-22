326b503339251f23ebe7a3e3d443854f7404ebeb
Jan Steinke <jan.steinke@stud.th-luebeck.de>
2021-12-22 16:30:25 +0100

    Funktionsfähige Schnittstelle zum Server implementiert. Connection-Klasse wird mit dem gewünschten URL-Pfad, der benötigten Anfragemethode und dem privaten Token initialisiert und stellt GETTER-Methode zur Verfügung, die Zugriff auf das Anfrageergebnis gibt. Quiz-Kategorien werden nun vom Server (oder von lokaler JSON-Datei) geladen und im Auswahlmenü angeboten.

--- src/main/java/de/gruppeo/wise2122_java_client/controllers/CCategory.java
+++ src/main/java/de/gruppeo/wise2122_java_client/controllers/CCategory.java
@@ -1,39 +1,77 @@
 package de.gruppeo.wise2122_java_client.controllers;
 
+import de.gruppeo.wise2122_java_client.Connection;
 import de.gruppeo.wise2122_java_client.ViewLoader;
+import de.gruppeo.wise2122_java_client.models.MCategory;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import org.springframework.boot.configurationprocessor.json.JSONException;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 
 public class CCategory {
 
-    private ViewLoader loader = new ViewLoader();
+    ViewLoader loader;
+    MCategory category;
 
     @FXML private BorderPane mainPane;
     @FXML private ComboBox combo_category_selectedCategory;
     @FXML private RadioButton radio_category_10Questions;
     @FXML private RadioButton radio_category_20Questions;
 
-    @FXML public void initialize() throws MalformedURLException {
-        combo_category_selectedCategory.getItems().add("Kategorie 1");
+    /**
+     * Initialisiert das ViewLoader-Objekt für
+     * den Maskenwechsel und etabliert eine neue
+     * Serververbindung zur Bereitstellung der
+     * Quiz-Kategorien.
+     *
+     * @throws Exception
+     */
+    public CCategory() throws Exception {
+        this.loader = new ViewLoader();
+
+        Connection connection = new Connection("/category", "GET" ,"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJNYXgiLCJpYXQiOjE2NDAwMDE3ODQsImV4cCI6MTY0MDYwNjU4NH0.nHm43HtiJdS3UG_ccxw-sg6DPsq-4LpQo8yYbMy4BMDCRHIbcqNECjlyl3Y2udL1bbhOQfLwQwgOy4epnN1NYQ");
+        this.category = new MCategory(connection.getServerResponse());
     }
 
+    /**
+     * Wird beim Aufruf der aktuellen Maske
+     * ausgeführt. Befüllt das Menü mit Kategorien.
+     */
+    @FXML public void initialize() {
+        for (String category : category.getCategories()) {
+            combo_category_selectedCategory.getItems().add(category);
+        }
+    }
+
+    /**
+     * Wird beim Klick auf "Zurück"-Button ausgeführt
+     * und wechselt die Maske zum Hauptmenü.
+     */
     public void onMouseClicked_back() {
         Stage stage = (Stage) mainPane.getScene().getWindow();
         stage.setScene(loader.getScene("main"));
         stage.show();
     }
 
+    /**
+     * Wird beim Klick auf "Gegner wählen" ausgeführt
+     * und wechselt die Maske zur Gegnerwahl.
+     */
     public void onMouseClicked_selectOpponent() {
         Stage stage = (Stage) mainPane.getScene().getWindow();
         stage.setScene(loader.getScene("opponent"));
         stage.show();
     }
 
+    /**
+     * Wird beim Klick auf "Quiz beitreten" ausgeführt
+     * und wechselt die Maske zur Lobby.
+     */
     public void onMouseClicked_joinQuiz() {
         Stage stage = (Stage) mainPane.getScene().getWindow();
         stage.setScene(loader.getScene("lobby"));
---
+++ src/main/java/de/gruppeo/wise2122_java_client/models/MCategory.java
@@ -0,0 +1,51 @@
+package de.gruppeo.wise2122_java_client.models;
+
+import org.springframework.boot.configurationprocessor.json.JSONArray;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MCategory {
+    private ArrayList<String> categories;
+
+    /**
+     * Initialisiert die Kategorien-Liste und
+     * führt die Methode zur Konvertierung der
+     * JSON-Zeichenkette aus.
+     *
+     * @param serverResponse
+     * @throws JSONException
+     */
+    public MCategory(String serverResponse) throws JSONException {
+        this.categories = new ArrayList<>();
+        parseJSON(serverResponse);
+    }
+
+    /**
+     * Liest die übergebene JSON-Zeichenkette ein,
+     * iteriert über alle Kategorien und speichert
+     * sie in eine Liste.
+     *
+     * @param json
+     * @throws JSONException
+     */
+    private void parseJSON(String json) throws JSONException {
+        JSONObject obj = new JSONObject(json);
+        JSONArray arr = obj.getJSONArray("categories");
+
+        for (int i = 0; i < arr.length(); i++) {
+            categories.add(arr.getJSONObject(i).getString("category_name"));
+        }
+    }
+
+    /**
+     * Gibt eine Liste aller
+     * Quiz-Kategorien zurück.
+     *
+     * @return Liste aller Quiz-Kategorien
+     */
+    public ArrayList<String> getCategories() {
+        return categories;
+    }
+}
