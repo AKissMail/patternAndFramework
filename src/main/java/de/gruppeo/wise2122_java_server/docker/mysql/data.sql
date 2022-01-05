1c031964573a88bd63e38663f902e57a1c885e59
Thomas K <thomas.krohnfuss@stud.th-luebeck.de>
2022-01-05 21:39:16 +0100

    Datenbankschema und Beispieldaten aktualisiert

--- src/main/java/de/gruppeo/wise2122_java_server/docker/mysql/data.sql
+++ src/main/java/de/gruppeo/wise2122_java_server/docker/mysql/data.sql
@@ -2,28 +2,33 @@
 INSERT INTO mibquizzz.player (playerid, username, password, currentscore, fk_quizHighscore, thumbnail, currentstatus)
 VALUES (1, 'todeskralle', 'fingerweg!', 100, 0, null, 'offline');
 INSERT INTO mibquizzz.player (playerid, username, password, currentscore, fk_quizHighscore, thumbnail, currentstatus)
-VALUES (1, 'bree luce', 't0d35kr4113', 500, 0, null, 'offline');
+VALUES (2, 'bree luce', 't0d35kr4113', 500, 0, null, 'offline');
 
 # 2. Beispielkategorien anlegen
-INSERT INTO mibquizzz.MCategory (quizcategoryid, categoryname)
-VALUES (1, 'Entwicklung');
-INSERT INTO mibquizzz.MCategory (quizcategoryid, categoryname)
-VALUES (2, 'Mediendesign');
-INSERT INTO mibquizzz.MCategory (quizcategoryid, categoryname)
-VALUES (3, 'IT-Sicherheit');
+INSERT INTO mibquizzz.category (quizcategoryid, categoryname)
+VALUES (1, 'Medieninformatik');
+INSERT INTO mibquizzz.category (quizcategoryid, categoryname)
+VALUES (2, 'Entwicklung');
+INSERT INTO mibquizzz.category (quizcategoryid, categoryname)
+VALUES (3, 'Mediendesign');
+INSERT INTO mibquizzz.category (quizcategoryid, categoryname)
+VALUES (4, 'IT-Sicherheit');
+INSERT INTO mibquizzz.category (quizcategoryid, categoryname)
+VALUES (5, 'Erdkunde');
 
 #3. Beispielfragen anlegen
 INSERT INTO mibquizzz.questions (quizquestionid, question, correctAnswer, falseAnswer1, falseAnswer2, falseAnswer3,
-                                     fk_quizCategory, difficulty)
+                                 difficulty, category_quizcategoryid)
 VALUES (1, 'Wo werden Laptops am häufigsten gestohlen?', 'An Bushaltestellen und Flughäfen', 'in Hotels',
-        'in Konferenzräumen', 'in der Mensa', 3, 'Fachkraft');
+        'in Konferenzräumen', 'in der Mensa', 'Fachkraft', 1);
 INSERT INTO mibquizzz.questions (quizquestionid, question, correctAnswer, falseAnswer1, falseAnswer2, falseAnswer3,
-                                     fk_quizCategory, difficulty)
+                                 difficulty, category_quizcategoryid)
 VALUES (2, 'Woran erkennt man eine sichere Webseite?', 'Am kleinen Schloss neben der URL',
         'Am optischen Gesamteindruck des Seite', 'Am Impressum: Der Firmensitz sollte in Deutschland sein',
-        'Wie aktuell die Seite ist', 3, 'Fachkraft');
+        'Wie aktuell die Seite ist', 'Fachkraft', 4);
 
 #4. HighscoreEntity Beispieldaten
-INSERT INTO mibquizzz.highscore (quizHighscoreID, fk_playerID, highscorepoints, lastUpdate) VALUES (1, 1, 500, '2021-12-08 12:16:45');
+INSERT INTO mibquizzz.highscore (quizHighscoreID, fk_playerID, highscorepoints, lastUpdate)
+VALUES (1, 1, 500, '2021-12-08 12:16:45');
 INSERT INTO mibquizzz.highscore (quizHighscoreID, fk_playerID, highscorepoints, lastUpdate) VALUES (2, 2, 350, '2021-12-10 22:25:08');
 
