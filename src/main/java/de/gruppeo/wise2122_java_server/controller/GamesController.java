6950591e5f9504ca7666ede4d7f3a4dced539ca3
321501 <KLNotbf22xMrt5VzFSAwdkmSbH+--+>
2022-01-15 21:18:08 +0100

    der GamesController.java ist nun fertig \o/

--- src/main/java/de/gruppeo/wise2122_java_server/controller/GamesController.java
+++ src/main/java/de/gruppeo/wise2122_java_server/controller/GamesController.java
@@ -1,19 +1,29 @@
 package de.gruppeo.wise2122_java_server.controller;
 
 import de.gruppeo.wise2122_java_server.model.*;
-import de.gruppeo.wise2122_java_server.repository.CategoryRepository;
-import de.gruppeo.wise2122_java_server.repository.GamesRepository;
-import de.gruppeo.wise2122_java_server.repository.PlayerRepository;
-import de.gruppeo.wise2122_java_server.repository.RoundsRepository;
+import de.gruppeo.wise2122_java_server.repository.*;
 import de.gruppeo.wise2122_java_server.request.DropAnswerRequest;
 import de.gruppeo.wise2122_java_server.request.NewGameRequest;
 import de.gruppeo.wise2122_java_server.request.UpdateGameRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static de.gruppeo.wise2122_java_server.model.Gamestatus.CLOSE;
+import static de.gruppeo.wise2122_java_server.model.Gamestatus.RUNNING;
+
+/**
+ * Das ist der GamesController hier werden alle api schnittstellen rund um ein Spiel behandelt.
+ * Hier gibt es:
+ * /games/create → ein Spiel erstellen
+ * /games/update → ein Spiel updaten (beitreten oder löschen)
+ * /games/dropAnswer → eine antwort abgeben
+ * /games/{id} → ein bestimmte spiel finden
+ * /games/all → all spiele finden
+ */
 @RestController
 @RequestMapping("/games")
 public class GamesController {
@@ -22,22 +32,33 @@ public class GamesController {
     private final PlayerRepository playerRepository;
     private final CategoryRepository categoryRepository;
     private final RoundsRepository roundsRepository;
+    private final QuestionsRepository questionsRepository;
+    private final HighscoreRepository highscoreRepository;
 
-
-    public GamesController(GamesRepository gamesRepository, PlayerRepository playerRepository, CategoryRepository categoryRepository, RoundsRepository roundsRepository) {
+    /**
+     * Constructor
+     */
+    public GamesController(GamesRepository gamesRepository, PlayerRepository playerRepository, CategoryRepository categoryRepository, RoundsRepository roundsRepository, QuestionsRepository questionsRepository, HighscoreRepository highscoreRepository) {
         this.gamesRepository = gamesRepository;
         this.playerRepository = playerRepository;
         this.categoryRepository = categoryRepository;
         this.roundsRepository = roundsRepository;
+        this.questionsRepository = questionsRepository;
+        this.highscoreRepository = highscoreRepository;
     }
-    // ein neues Game anlegen
+    /**
+     * Diese Methode nimmt eine Anfrage für ein neues Spiel an und erstellt und gibt diese zurück.
+     * @param newGameRequest Das ist das Request mapping
+     * @return das Spiel mit allen Fragen
+     */
     @PostMapping("/create")
     public ResponseEntity<GamesEntity> createGame(@RequestBody NewGameRequest newGameRequest) {
         Optional<PlayerEntity> playerOne = playerRepository.findByUsername(newGameRequest.getUsername());
         Optional<CategoryEntity> gameCategory = categoryRepository.findByCategoryname(newGameRequest.getCategory());
         Optional<RoundsEntity> gameRound = roundsRepository.findByRounds(newGameRequest.getRounds());
-
+        List<QuestionsEntity> questionOfCategories = questionsRepository.findByCategory_CategorynameAllIgnoreCase(newGameRequest.getCategory());
         if (playerOne.isPresent() && gameCategory.isPresent() && gameRound.isPresent()) {
+            List<QuestionsEntity> randomQuestions = getRandomQuestions(questionOfCategories, newGameRequest.getRounds());
             GamesEntity newGame = new GamesEntity();
             newGame.setGamestatus(Gamestatus.OPEN);
             newGame.setPlayerone(playerOne.get());
@@ -46,6 +67,7 @@ public class GamesController {
             newGame.setPlayertwoscore(0);
             newGame.setPlayertworound(0);
             newGame.setCategory(gameCategory.get());
+            newGame.setQuestions(randomQuestions);
             newGame.setRounds(gameRound.get());
             GamesEntity createdGame = gamesRepository.save(newGame);
             return ResponseEntity.ok(createdGame);
@@ -53,26 +75,18 @@ public class GamesController {
             return ResponseEntity.badRequest().build();
         }
     }
-
     /**
-     * Aktualisiere ein Spiel in der Datenbank
-     * <p>
      * Mit dieser Methode kann ein Spiel in der Datenbank aktualisiert werden.
-     *
-     * @param updateGameRequest Man kann folgende Param übergeben: gamesid, playerone, playertwo, status
+     * @param updateGameRequest Man kann folgende Param übergeben: games id, player one, player two, status
      * @return GamesEntity
      */
     @PutMapping("/update")
     public ResponseEntity<GamesEntity> updateGame(@RequestBody UpdateGameRequest updateGameRequest) {
         Optional<GamesEntity> updateGame = gamesRepository.findById(updateGameRequest.getGamesid());
-        // Prüfe, ob übergebene Gamesid überhaupt existiert
         if (updateGame.isEmpty()) {
             return ResponseEntity.badRequest().build();
         }
-        // Var vom Typ GamesEntity initiieren
         GamesEntity updatedGame;
-
-        // Setze Spielerstatus oder, wenn null, werfe den Spieler aus dem Spiel
         switch (updateGameRequest.getPlayerone() + "-" + updateGameRequest.getPlayertwo()) {
             case "null-":
                 updateGame.get().setGamestatus(Gamestatus.valueOf(updateGameRequest.getStatus()));
@@ -111,71 +125,121 @@ public class GamesController {
                 }
         }
     }
-    /* @todo hier ist noch ein Bug bei dem ich nicht weiter komme: Ein Vergleich, der checkt ob beide Spieler Antworten gegeben
-          haben hat und den Status aktualisiert...
-    // eine Antwort in ein Game eintragen
+    /**
+     * Nimmt die Antworten entgegen und verwaltet das Spiel (Antworten gezählt, Fragen entgegengenommen und der Highscore wegschreibe).
+     * @param dropAnswerRequest das ist das mapping
+     * @return den Aktuellen stand, solange diese auf RUNNING steht.
+     */
     @PutMapping("/dropAnswer")
-    public ResponseEntity<GamesEntity> dropAnswer(@RequestBody DropAnswerRequest dropAnswerRequest){
-        Optional<PlayerEntity> player = playerRepository.findByUsername(dropAnswerRequest.getUsername());
+    public ResponseEntity<GamesEntity> dropAnswer(@RequestBody DropAnswerRequest dropAnswerRequest) {
         Optional<GamesEntity> updateGame = gamesRepository.findById(dropAnswerRequest.getGamesid());
-
-        if (player.isPresent() && updateGame.isPresent()) {
-         // if (true){ <- wie kann ich hier den player 1 von player 2 unterscheiden? Mir ist nichts eingefallen was auch funktioniert hat.
-               if (!dropAnswerRequest.isAnswers()){ // mach die if condition so sinn?
-                   updateGame.get().setPlayeronescore(updateGame.get().getPlayeronescore()+ scoreCalculator(dropAnswerRequest.getTime()));
-                   updateGame.get().setPlayeroneround(updateGame.get().getPlayeroneround()+1);
-                   GamesEntity createGame = gamesRepository.save(updateGame.get());
-                   return ResponseEntity.ok(createGame);
-               }else{
-                   updateGame.get().setPlayeroneround(updateGame.get().getPlayeroneround()+1);
-                   GamesEntity createGame = gamesRepository.save(updateGame.get());
-
-                   return ResponseEntity.ok(createGame);
-               }
-           }
-           else{
-               if (!dropAnswerRequest.isAnswers()){
-                   updateGame.get().setPlayertwoscore(updateGame.get().getPlayeronescore()+scoreCalculator(dropAnswerRequest.getTime()));
-                   updateGame.get().setPlayertworound(updateGame.get().getPlayeroneround()+1);
-                   GamesEntity createGame = gamesRepository.save(updateGame.get());
-                   return ResponseEntity.ok(createGame);
-               }else{
-                   updateGame.get().setPlayertworound(42);  //(updateGame.get().getPlayeroneround()+1);
-                   GamesEntity createGame = gamesRepository.save(updateGame.get());
-                   return ResponseEntity.ok(createGame);
-               }
-           }
+        if (updateGame.isPresent()) {
+            if (updateGame.get().getGamestatus() == RUNNING) {
+                if (dropAnswerRequest.isPlayerone()) {
+                    if (dropAnswerRequest.isAnswers()) {
+                        updateGame.get().setPlayeronescore(updateGame.get().getPlayeronescore() + scoreCalculator(dropAnswerRequest.getTime()));
+                    }
+                    updateGame.get().setPlayeroneround(updateGame.get().getPlayeroneround() + 1);
+                    if (checkGameCount(updateGame)) {
+                        updateGame.get().setGamestatus(CLOSE);
+                        int currentScore = updateGame.get().getPlayeronescore();
+                        int highscore = highscoreRepository.findByPlayer_Username(updateGame.get().getPlayerone().getUsername()).get().highscorepoints;
+                        if (currentScore > highscore) {
+                            highscoreRepository.findByPlayer_Username(updateGame.get().getPlayertwo().getUsername()).get().setHighscorepoints(updateGame.get().getPlayertwoscore());
+                            highscoreRepository.findByPlayer_Username(updateGame.get().getPlayerone().getUsername()).get().setHighscorepoints(currentScore);
+                        }
+                    }
+                    GamesEntity createGame = gamesRepository.save(updateGame.get());
+                    return ResponseEntity.ok(createGame);
+                } else {
+                    if (dropAnswerRequest.isAnswers()) {
+                        updateGame.get().setPlayertwoscore(updateGame.get().getPlayertwoscore() + scoreCalculator(dropAnswerRequest.getTime()));
+                    }
+                    updateGame.get().setPlayertworound(updateGame.get().getPlayertworound() + 1);
+                    if (checkGameCount(updateGame)) {
+                        updateGame.get().setGamestatus(CLOSE);
+                        int currentScore = updateGame.get().getPlayertwoscore();
+                        int highscore = highscoreRepository.findByPlayer_Username(updateGame.get().getPlayertwo().getUsername()).get().highscorepoints;
+                        if (currentScore > highscore) {
+                            highscoreRepository.findByPlayer_Username(updateGame.get().getPlayertwo().getUsername()).get().setHighscorepoints(currentScore);
+                            highscoreRepository.findByPlayer_Username(updateGame.get().getPlayerone().getUsername()).get().setHighscorepoints(updateGame.get().getPlayertwoscore());
+                        }
+                    }
+                    GamesEntity createGame = gamesRepository.save(updateGame.get());
+                    return ResponseEntity.ok(createGame);
+                }
+            }else{
+                return ResponseEntity.badRequest().build();
+            }
         } else {
             return ResponseEntity.badRequest().build();
         }
     }
-
+    /**
+     * Die Methode gibt eine Liste offener Spiele zurück
+     * @return Liste offener
      */
-
-    // Offene Games
     @GetMapping("/open")
     public List<GamesEntity> findByGamestatus() {
         return gamesRepository.findByGamestatus(Gamestatus.OPEN);
     }
-
-    // Single Game
+    /**
+     * Diese Methode gibt ein bestimmtes Spiel zurück anhand einer ID
+     * @param id die ID des zu suchen
+     * @return das gesuchte Spiel
+     */
     @GetMapping("/{id}")
-    public Optional<GamesEntity> findById(
-            @PathVariable Long id) {
+    public Optional<GamesEntity> findById(@PathVariable Long id) {
         return gamesRepository.findById(id);
     }
-
-    // Alle Games
+    /**
+     * Mit dieser Methode werden alle Spiele gefunden und zurück übermittelt
+     * @return die Spiele
+     */
     @GetMapping("/all")
     public List<GamesEntity> index() {
         return gamesRepository.findAll();
     }
-
-    private int scoreCalculator (int time) {
+    /**
+     * Dieser Methode werden die Punkte zurückgegeben
+     * @param time Zeit die der Spieler gebraucht hat
+     * @return Die Punkte
+     */
+    private int scoreCalculator(int time) {
         if (time <= 0) {
-            return 0;
+            return 1;
         } else {
             return time / 100;
         }
+    }
+    /**
+     * Diese Methode überprüft, ob das Spiel zu Ende ist.
+     * @param gamesRepository das zu prüfende Spiel
+     * @return true - Spiel ist zu Ende | false - Spiel noch nicht zu Ende
+     */
+    public boolean checkGameCount(Optional<GamesEntity> gamesRepository) {
+        if (gamesRepository.isPresent()) {
+            int playerOne = gamesRepository.get().getPlayeroneround();
+            int playerTwo = gamesRepository.get().getPlayertworound();
+            int gameSize = gamesRepository.get().getRounds().getRounds();
+            return playerTwo == gameSize && playerOne == gameSize;
+        }
+        return false;
+    }
+    /**
+     * Wählt aus ein Set von Fragen, eine bestimme anzahl an Fragen zufällig aus.
+     * @param questions         das Set an Fragen
+     * @param numberOfQuestions die anzahl der Fragen die ausgewählt wurden´
+     * @return Die Liste der ausgewählten Fragen
+     */
+    public List<QuestionsEntity> getRandomQuestions(List<QuestionsEntity> questions, int numberOfQuestions) {
+        List<QuestionsEntity> randomQuestions = new ArrayList<>();
+        List<QuestionsEntity> copy = new ArrayList<>(questions);
+
+        SecureRandom rand = new SecureRandom();
+        for (int i = 0; i < Math.min(numberOfQuestions, questions.size()); i++) {
+            randomQuestions.add(copy.remove(rand.nextInt(copy.size())));
+        }
+        return randomQuestions;
     }
 }
