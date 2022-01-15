f0f80b6c1af51ddfd6bd08e0a796f948364b637f
321501 <KLNotbf22xMrt5VzFSAwdkmSbH+--+>
2022-01-15 21:26:09 +0100

    Hier sind noch ein paar javadoc kommentare.

--- src/main/java/de/gruppeo/wise2122_java_server/request/DropAnswerRequest.java
+++ src/main/java/de/gruppeo/wise2122_java_server/request/DropAnswerRequest.java
@@ -5,7 +5,7 @@ import lombok.Data;
 /**
  * mapping für die das Sende der Antworten
  * games id = die ID des game
- * player one = Wenn der Player one antwortet -> true | Player tow antwortet -> false
+ * player one = Wenn der Player one antwortet → true | Player tow antwortet → false
  * time = die verbleibende Zeit bis die 10 Sekunden voll sind.
  */
 @Data
