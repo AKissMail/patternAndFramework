cbbd65a3554f4a8152e8ba4a259bb6f6e6982400
Thomas Krohnfuß <thomas.krohnfuss@stud.th-luebeck.de>
2022-01-15 20:55:51 +0100

    Highscorereset in Client App funktioniert nun

--- src/main/java/de/gruppeo/wise2122_java_client/models/MHighscore.java
+++ src/main/java/de/gruppeo/wise2122_java_client/models/MHighscore.java
@@ -7,13 +7,15 @@ public class MHighscore {
 
     private int quizhighscoreid;
     public int highscorepoints;
+    private String playername;
     private String lastupdate;
 
     public MHighscore() {}
 
-    public MHighscore(int quizhighscoreid, int highscorepoints, String lastupdate) {
+    public MHighscore(int quizhighscoreid, int highscorepoints, String playername, String lastupdate) {
         this.quizhighscoreid = quizhighscoreid;
         this.highscorepoints = highscorepoints;
+        this.playername = playername;
         this.lastupdate = lastupdate;
     }
 
