17e2bf3975aa21c3082cf68a38eefc6b6c0feed1
321501 <KLNotbf22xMrt5VzFSAwdkmSbH+--+>
2022-01-15 19:58:20 +0100

    bugfix

--- src/main/java/de/gruppeo/wise2122_java_server/repository/QuestionsRepository.java
+++ src/main/java/de/gruppeo/wise2122_java_server/repository/QuestionsRepository.java
@@ -1,6 +1,7 @@
 package de.gruppeo.wise2122_java_server.repository;
 
 import de.gruppeo.wise2122_java_server.model.QuestionsEntity;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,5 @@ import java.util.List;
 
 @Repository
 public interface QuestionsRepository extends JpaRepository<QuestionsEntity, Long> {
-    //    List<PlayerEntity> findByCurrentstatusAllIgnoreCase(String CurrentStatus);
     List<QuestionsEntity> findByCategory_CategorynameAllIgnoreCase(String Category);
 }
