660f45fcfe80ce935c59467b0351f78d18e1197e
321501 <KLNotbf22xMrt5VzFSAwdkmSbH+--+>
2022-01-26 16:46:30 +0100

    überarbeitung der Controller

---
+++ src/main/jswebclient/js/controller/C_updatePicture.js
@@ -0,0 +1,33 @@
+import * as apiCalls from '../model/M_model.js';
+import * as mainMenu from '../view/V_mainMenu.js';
+
+/**
+ *
+ */
+export function base64Picture() {
+    const toBase64 = file => new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+
+    $('#button').click(function() {
+        const uploadedFile = document.querySelector('#inputFile').files[0];
+        toBase64(uploadedFile)
+            .then(res => {
+                let cutRes = res.split(',');
+                let data = cutRes[1];
+                console.log(data);
+                apiCalls.updatePicture(data, function (){
+                    console.log(res);
+                    mainMenu.show();
+                });
+
+            })
+            .catch(err => {
+                alert("Das hat leider nicht geklappt" + err);
+                console.log(err);
+            })
+    });
+}
